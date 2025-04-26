68376cf305aaffc81c9f24e32fee8bbbecb9cace
mizhikeji <xsfenglong@gmail.com>
2025-04-26 17:51:03 +0800

    Update start script

---
+++ bin/tools/register.sh
@@ -0,0 +1,8 @@
+npm install
+node register.js http://127.0.0.1:8979 key-1.json 5000000000
+node register.js http://127.0.0.1:8979 key-2.json 5000000000
+node register.js http://127.0.0.1:8979 key-3.json 5000000000
+node register.js http://127.0.0.1:8979 key-4.json 5000000000
+node register.js http://127.0.0.1:8979 key-5.json 5000000000
+node register.js http://127.0.0.1:8979 key-6.json 5000000000
+node register.js http://127.0.0.1:8979 key-7.json 5000000000
