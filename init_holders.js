d396455675494dbd08b5063c91bef4aece4d6dcb
mizhikeji <xsfenglong@gmail.com>
2025-04-26 17:22:01 +0800

    change  init  balance of init_holders

--- init_holders.js
+++ init_holders.js
@@ -2,31 +2,31 @@ const web3 = require("web3")
 const init_holders = [
     {
         address: "0xa4eCD346d065827d303E95934eD712E978693d97",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0x2Af1516cBA4b8aBd55E98eD2AABF91D367F02734",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0x217d71773cAF8916484800B959248DAfC44A0629",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0x82F74B5Adc6Cc4aCAc54D80a2559317284fE2b87",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0x68786fe80F10449C6cF3aCD97299fAcf15050721",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0xAB8F65a007481a346D907E9BB4c2a83bdb8e9aA9",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0xA4458700cfadb165FA7ED5E650835F214482956f",
-        balance: web3.utils.toBN("1000000000000000000000000000").toString("hex") // 1e27
+        balance: web3.utils.toBN("10000000000000000000000000000").toString("hex") // 1e27
      },
      {
         address: "0xa2c45104F9c6BC2A23C32e88bac371ce637668cE",
